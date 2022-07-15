6919cd2e6b4422e80a5ef46ec3c259b8b2ab64f8
theiou <temicho_0@mail.ru>
2022-07-15 16:01:23 +1000

    add view for categories in the left section

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { TestData } from '../Data/TestData';
+import { Category } from '../models/Category';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,10 @@ import { Injectable } from '@angular/core';
 export class DataHandlerService {
 
   constructor() { }
+
+  getCategories(): Category[]{
+    return TestData.categories;
+  }
+
+  
 }
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/Category';
+import { DataHandlerService } from 'src/app/services/data-handler.service';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
   }
 
 }
