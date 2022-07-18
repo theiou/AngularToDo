35b34db3d8311efb6b192149ab7159b43e22c4e3
theiou <temicho_0@mail.ru>
2022-07-18 21:09:17 +1000

    add sorting and pagination on task table

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,18 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from 'src/app/services/data-handler.service';
 import {Task} from "../../models/Task";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
   displayedColumns: String[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
   dataSource!: MatTableDataSource<Task>;
-  tasks: Task[] = [];
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
+  tasks!: Task[];
 
   constructor(private dataHandler: DataHandlerService) {
   }
@@ -23,19 +30,48 @@ export class TasksComponent implements OnInit {
     this.refreshTable();
   }
 
-  toggleCompleted(task: Task):void {
+  ngAfterViewInit(): void {
+    this.addTableObjects();
+  }
+
+  toggleCompleted(task: Task): void {
     task.completed = !task.completed;
   }
 
-  getPriorityColor(task: Task){
-    if (task.priority && task.priority.color){
+  getPriorityColor(task: Task) {
+    if (task.priority && task.priority.color) {
       return task.priority.color;
     }
     return "#fff";
   }
 
-  refreshTable(): void {
+  refreshTable() {
     this.dataSource.data = this.tasks;
+    this.addTableObjects();
+
+    // @ts-ignore
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
+        }
+        case 'title': {
+          return task.title;
+        }
+      }
+    };
+
+  }
+
+  addTableObjects() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
